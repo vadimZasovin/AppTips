8cc126bc836adb3433f91861853b6de96542f694
vadimZasovin <vzasovin@gmail.com>
2018-03-06 16:59:03 +0300

    updated Util class

--- app-tips/src/main/java/com/imogene/apptips/Util.java
+++ app-tips/src/main/java/com/imogene/apptips/Util.java
@@ -1,18 +1,13 @@
 package com.imogene.apptips;
 
-import android.os.Build;
-
 /**
  * Created by Admin on 25.04.2016.
  */
 class Util {
 
+    // TODO remove this function
     static float sinDegrees(float degrees){
         return (float) Math.sin(Math.toRadians(degrees));
-    }
-
-    static boolean checkApiVersion(int apiVersion){
-        return Build.VERSION.SDK_INT >= apiVersion;
     }
 
     static void checkNonNullParameter(Object parameter, String name){
